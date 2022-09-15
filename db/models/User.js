37348ebc9c168296085238a469e78035af934170
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-09-15 19:01:35 +0400

    model fix

--- db/models/User.js
+++ db/models/User.js
@@ -1,15 +1,19 @@
 module.exports = (conn, DataTypes) => {
-  const User = conn.define("users", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const User = conn.define(
+    "users",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      nickname: { type: DataTypes.STRING, unique: true, allowNull: false },
+      email: { type: DataTypes.STRING, unique: true, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
     },
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    nickname: { type: DataTypes.STRING, unique: true, allowNull: false },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-  });
+    { timestamps: false }
+  );
   return User;
 };
